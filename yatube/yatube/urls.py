948b6ed0c4b5d92c3f37b0912ed2848407769707
Алексей Федотов <fedotov.alexm@gmail.com>
2022-07-17 18:07:06 +0300

    debug added

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 
-
 handler404 = 'core.views.page_not_found'
 handler403 = 'core.views.csrf_failure'
 handler500 = 'core.views.server_error'
@@ -17,6 +16,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    import debug_toolbar
+
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
